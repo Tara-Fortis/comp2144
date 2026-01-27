ca32197d1113e87382d7712ed2519f84bc4b7e65
Tineil_A <tine6573@gmail.com>
2026-01-27 17:20:40 -0500

    added and upscaled low poly tree, enabled webxr session

--- lesson-4/js/village.js
+++ lesson-4/js/village.js
@@ -102,8 +102,20 @@ const createScene = async function() {
         }, audioEngine);
     }
     initAudio();
-    // STEP 15a: Set the above createScene() function to async (important, or this will not work)
-    // STEP 15b: Create the xrHelper to allow the visitor to choose WebXR if they are able and they'd like
+    // STEP 15a: Get a 3D model of a tree (https://free3d.com/3d-model/low_poly_tree-816203.html)
+    // STEP 15b: Convert mesh and material data to .glb file (https://convert3d.org)
+    // STEP 15c: Drop the tree into the scene
+    const tree = BABYLON.SceneLoader.ImportMeshAsync("", "./meshes/", "low_poly_tree/Lowpoly_tree_sample.glb").then((result) => {
+        result.meshes[0].position.x = -2.5;
+        result.meshes[0].position.y = 0;
+        result.meshes[0].position.z = -2.5;
+        result.meshes[0].scaling = new BABYLON.Vector3(150, 150, 150);
+    });
+
+
+
+    // STEP 17a: Set the above createScene() function to async (important, or this will not work)
+    // STEP 17b: Create the xrHelper to allow the visitor to choose WebXR if they are able and they'd like
     const xr = await scene.createDefaultXRExperienceAsync({
         floorMeshes: [ground],
         optionalFeatures: true
